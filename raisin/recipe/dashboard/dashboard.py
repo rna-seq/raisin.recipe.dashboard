bdf2c5d60613c0df01832bcf9e295e2440b8bf34
Maik R��der <maikroeder@gmail.com>
2012-02-11 19:41:39 +0000

    =docstrings and implement further methods

--- raisin/recipe/dashboard/dashboard.py
+++ raisin/recipe/dashboard/dashboard.py
@@ -1,12 +1,48 @@
+"""
+Render a dashboard given settings defined in the buildout recipe.
+"""
+
+import csv
+
+
+def dashboard(title,
+              description,
+              lines,
+              output_file,
+              dimensions,
+              rows,
+              cols,
+              filters):
+    """
+    Create the dashboard page.
+    """
+    #html = render_table(title, description, lines, dimensions, rows, cols,
+    #filters)
+    html = ""
+    static_file = open(output_file, "w")
+    static_file.write(html)
+    static_file.close()
+
+
+def get_lines(file_name):
+    """
+    Get the lines from the csv file.
+    """
+    lines = csv.DictReader(open(file_name, 'r'),
+                           delimiter='\t',
+                           skipinitialspace=True)
+    return lines
+
+
 def get_filters(options):
     """
     Parse the filters given in the options.
     """
     filters = {}
     if 'filters' in options:
-        for filter in options['filters'].split('\n'):
-            if ':' in filter:
-                key, value = filter.split(':')
+        for filter_line in options['filters'].split('\n'):
+            if ':' in filter_line:
+                key, value = filter_line.split(':')
                 key = key.strip()
                 value = value.strip()
                 if len(key) > 0 and len(value) > 0:
@@ -17,11 +53,13 @@ def get_filters(options):
 
 
 def main(options, buildout):
+    """
+    Fetch the buildout options and create the dashboard.
+    """
     csv_file = options['csv_file']
     output_file = options['output_file']
     rows = options['rows'].split('\n')
     cols = options['cols'].split('\n')
-
 
     filters = get_filters(options)
 
@@ -32,8 +70,9 @@ def main(options, buildout):
     for col in cols:
         dimensions[col] = buildout[options['vocabulary']][col]
 
-    title = options['title']        
-    description = options['description']        
+    title = options['title']
+    description = options['description']
 
-    dashboard(title, description, lines, output_file, dimensions, rows, cols, filters)
-    return output_file    
+    dashboard(title, description, lines, output_file, dimensions, rows, cols,
+              filters)
+    return output_file
