df5e3acf03ac7cb0b9b4479e13883b52438d009d
Maik R��der <maikroeder@gmail.com>
2012-02-11 17:57:17 +0000

    =add

--- raisin/recipe/dashboard/tests/test_cube.py
+++ raisin/recipe/dashboard/tests/test_cube.py
@@ -13,6 +13,14 @@ class CubeTests(unittest.TestCase):
         cube = Cube(accessions, rows, cols)
         self.failUnless(cube.get_rows() == [])
         self.failUnless(cube.get_cols() == [])
+        self.failUnlessRaises(KeyError, cube.get_dimension_values, None)
+        self.failUnless(cube.get_row_values() == [])
+        self.failUnless(list(cube.get_col_values()) == [()])
+        accession_id = None
+        files = []
+        rows_key = None
+        cols_key = None
+        cube.add_accession_files(accession_id, files, rows_key, cols_key)
 
 
 def test_suite():
