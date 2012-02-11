bdc9dfbdd7087dabc6bf7005c29250e97b19b955
Maik R��der <maikroeder@gmail.com>
2012-02-11 19:40:53 +0000

    =docstrings

--- raisin/recipe/dashboard/recipe.py
+++ raisin/recipe/dashboard/recipe.py
@@ -1,14 +1,30 @@
-import dashboard
+"""
+Buildout recipe raisin.recipe.dashboard
+"""
+from raisin.recipe.dashboard import dashboard
+
 
 class Recipe(object):
+    """
+    Buildout recipe for creating dashboards.
+    """
 
     def __init__(self, buildout, name, options):
+        """
+        Store the parameters for the buildout recipe.
+        """
         self.buildout = buildout
         self.name = name
         self.options = options
 
     def install(self):
+        """
+        Install the dashboard.
+        """
         return dashboard.main(self.options, self.buildout)
-        
+
     def update(self):
+        """
+        Install the updated dashboard.
+        """
         return self.install()
