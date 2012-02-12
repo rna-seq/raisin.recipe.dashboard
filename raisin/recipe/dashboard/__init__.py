5b83528d4253707342b46c1ce76984798bac2028
Maik R��der <maikroeder@gmail.com>
2012-02-12 19:16:26 +0000

    =move recipe to __init__.py

--- raisin/recipe/dashboard/__init__.py
+++ raisin/recipe/dashboard/__init__.py
@@ -1,3 +1,31 @@
 # -*- coding: utf-8 -*-
-"""Recipe raisin.recipe.dashboard"""
-from raisin.recipe.dashboard.recipe import Recipe
+"""
+Buildout recipe raisin.recipe.dashboard
+"""
+from raisin.recipe.dashboard import dashboard
+
+
+class Recipe(object):
+    """
+    Buildout recipe for creating dashboards.
+    """
+
+    def __init__(self, buildout, name, options):
+        """
+        Store the parameters for the buildout recipe.
+        """
+        self.buildout = buildout
+        self.name = name
+        self.options = options
+
+    def install(self):
+        """
+        Install the dashboard.
+        """
+        return dashboard.main(self.options, self.buildout)
+
+    def update(self):
+        """
+        Install the updated dashboard.
+        """
+        return self.install()
