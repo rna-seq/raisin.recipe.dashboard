b61828ad854c6653479e4c9ade55d0ddc1cc27a4
Maik R��der <maikroeder@gmail.com>
2012-03-15 14:11:14 +0000

    =docstrings

--- raisin/recipe/dashboard/replicates.py
+++ raisin/recipe/dashboard/replicates.py
@@ -4,20 +4,29 @@ from raisin.recipe.dashboard.cube import Cube
 class Renderer:
 
     def __init__(self, output):
+        """
+        Store the output to render to
+        """
         self.output = output
 
     def render(self, header, replicate, afile, number):
+        """
+        Render the cell depending on the header
+        """
         if header == "file_location":
             html = self.file_location(header, replicate, afile, number)
         elif header in ["genome_version"]:
-            html = self.genome_version(header, replicate, afile, number)
+            html = self.genome_version(afile)
         elif header in ["annotation_version"]:
-            html = self.annotation_version(header, replicate, afile, number)
+            html = self.annotation_version(afile)
         else:
-            html = self.header(header, replicate, afile, number)
+            html = self.header(header, afile)
         self.output.write(html)
 
     def file_location(self, header, replicate, afile, number):
+        """
+        Render the file location
+        """
         if len(replicate[1]) == 1:
             view = "Raw data"
         else:
@@ -33,32 +42,50 @@ class Renderer:
         template = """<td><a href="%s">%s</a></td>\n"""
         return template % (afile[header], view)
 
-    def genome_version(self, header, replicate, afile, number):
+    def genome_version(self, afile):
+        """
+        Render the genome version
+        """
         template = ("""<td><a href="%s" title="%s">"""
                     """%s</a></td>\n""")
         return template % (afile["genome_url"],
                            afile["genome"],
                            afile["genome_version"])
 
-    def annotation_version(self, header, replicate, afile, number):
+    def annotation_version(self, afile):
+        """
+        Render the annotation version
+        """
         template = ("""<td><a href="%s" title="%s">"""
                     """%s</a></td>\n""")
         return template % (afile["annotation_url"],
                            afile["annotation"],
                            afile["annotation_version"])
 
-    def header(self, header, replicate, afile, number):
+    def header(self, header, afile):
+        """
+        Render the header
+        """
         return "<td>%s</td>\n" % afile[header]
 
 
 class Replicates(Cube):
+    """
+    Replicates cube
+    """
 
     def __init__(self, accessions, rows, cols):
+        """
+        Keep the accessions for the replicates
+        """
         self.replicates = {}
         Cube.__init__(self, accessions, rows, cols)
 
-    def add_accession_files(self, accession_id, files, rows_key, cols_key):
-        Cube.add_accession_files(self, accession_id, files, rows_key, cols_key)
+    def add_accession_files(self, files, accession_id, rows_key, cols_key):
+        """
+        Add accession files
+        """
+        Cube.add_accession_files(self, files, accession_id, rows_key, cols_key)
         key = (tuple(rows_key), tuple(cols_key))
         if key in self.replicates:
             self.replicates[key].append((accession_id, files))
@@ -66,6 +93,9 @@ class Replicates(Cube):
             self.replicates[key] = [(accession_id, files)]
 
     def produce_table(self, output, key, attribute_categories):
+        """
+        Produce the table
+        """
         replicates = self.replicates[key]
 
         remove = self.dimensions
