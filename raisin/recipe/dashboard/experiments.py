2360bad482b91d67b53df6cbb03f064d87e3bf3b
Maik R��der <maikroeder@gmail.com>
2012-03-15 14:09:44 +0000

    =docstrings

--- raisin/recipe/dashboard/experiments.py
+++ raisin/recipe/dashboard/experiments.py
@@ -1,20 +1,36 @@
+"""
+Experiments cube
+"""
+
 from raisin.recipe.dashboard.cube import Cube
 from raisin.recipe.dashboard.replicates import Replicates
 
 
 class Experiments(Cube):
+    """
+    Experiment cube holding the replicates.
+    """
 
     def __init__(self, accessions, rows, cols):
+        """
+        Take the accessions and derive the replicates.
+        """
         Cube.__init__(self, accessions, rows, cols)
         rows = self.dimensions
         cols = ["replicate"]
         self.replicates = Replicates(accessions, rows, cols)
 
-    def add_accession_files(self, accession_id, files, rows_key, cols_key):
-        for file in files:
-            if file is None:
+    def add_accession_files(self, files, accession_id, rows_key, cols_key):
+        """
+        Add the accession files to the cube.
+        """
+        for afile in files:
+            if afile is None:
                 raise AttributeError
-        Cube.add_accession_files(self, accession_id, files, rows_key, cols_key)
+        Cube.add_accession_files(self, files, accession_id, rows_key, cols_key)
 
     def get_replicates(self):
+        """
+        Return the replicates.
+        """
         return self.replicates
